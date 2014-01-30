a6533334523dc8915598ebd25006af5e95d88e03
mertvetsov <mertvetsov@yandex-team.ru>
2014-01-30 21:00:24 +0400

    YMAInstanceManager added.

---
+++ cps-sdk/SDK/ymcpssdk.h
@@ -0,0 +1,19 @@
+//
+//  ymcpssdk.h
+//  cps-sdk
+//
+//  Created by mertvetcov on 30.01.14.
+//  Copyright (c) 2014 Yandex.Money. All rights reserved.
+//
+
+#import "YMASession.h"
+#import "YMAAsc.h"
+#import "YMAMoneySource.h"
+#import "YMAConstants.h"
+#import "YMABaseRequest.h"
+#import "YMABaseResponse.h"
+#import "YMAExternalPaymentRequest.h"
+#import "YMAProcessExternalPaymentRequest.h"
+#import "YMAExternalPaymentResponse.h"
+#import "YMAProcessExternalPaymentResponse.h"
+
