520b4f16dcaf1429a71c533a45eed8a85b63f405
Александр Мертвецов <amertvetsov@yandex.ru>
2014-03-21 16:57:15 +0700

    a bit of refactoring

--- cps-sdk/SDK/YMASession.m
+++ cps-sdk/SDK/YMASession.m
@@ -15,7 +15,7 @@ typedef NS_ENUM(NSInteger, YMAConnectHTTPStatusCodes) {
     YMAStatusCodeInternalServerErrorHTTP = 500
 };
 
-static NSString *const kInstanceUrl = @"https://polo.yandex.ru/api/instance-id";
+static NSString *const kInstanceUrl = @"https://money.yandex.ru/api/instance-id";
 
 static NSString *const kParameterInstanceId = @"instance_id";
 static NSString *const kParameterClientId = @"client_id";
