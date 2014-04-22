e1387af416910cbc46e586957e8d32c6eab10746
mertvetsov <mertvetsov@yandex-team.ru>
2014-04-22 19:14:31 +0400

    YandexMoneySDK-example added

---
+++ Example/YandexMoneySDK-example/ViewController.m
@@ -0,0 +1,61 @@
+//
+//  ViewController.m
+//  ios-example
+//
+//  Created by mertvetcov on 07.02.14.
+//  Copyright (c) 2014 Yandex.Money. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property(nonatomic, strong) UIButton *doPaymentButton;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    
+    [self.doPaymentButton setTitle: @"test payment" forState:UIControlStateNormal];
+    [self.doPaymentButton setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
+    
+    [self.view addSubview:self.doPaymentButton];
+    
+    [self.doPaymentButton addTarget:self action:@selector(doTestPayment) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)doTestPayment {
+    
+}
+
+- (UIButton *)doPaymentButton {
+    if (!_doPaymentButton) {
+        CGRect buttonRect = CGRectMake(0, self.view.frame.size.height/2, self.view.frame.size.width, 44);
+        
+        _doPaymentButton = [[UIButton alloc] initWithFrame:buttonRect];
+        _doPaymentButton.backgroundColor = [UIColor whiteColor];
+    }
+    
+    return _doPaymentButton;
+}
+
+- (void)dealloc {
+    [_doPaymentButton release];
+
+    [super dealloc];
+}
+
+@end
