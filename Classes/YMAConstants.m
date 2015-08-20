d22dbef7cda6da6880cf41175d7861444162fc5b
Yury Vyazov <vjazov@yamoney.ru>
2015-08-20 12:10:50 +0300

    Label param added to Transfer

--- Classes/YMAConstants.m
+++ Classes/YMAConstants.m
@@ -12,6 +12,7 @@ NSString *const YMAP2PPaymentParameterTo = @"to";
 NSString *const YMAP2PPaymentParameterMessage = @"message";
 NSString *const YMAP2PPaymentParameterAmount = @"amount";
 NSString *const YMAP2PPaymentParameterAmountDue = @"amount_due";
+NSString *const YMAP2PPaymentParameterLabel = @"label";
 
 NSString *const YMAErrorDomainYaMoneyAPI = @"yaMoneyError";
 NSString *const YMAErrorDomainUnknown = @"technicalError";
