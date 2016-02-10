4c826c8c7c1ed7a62b34b372d1a6777e2d933297
Юрий М. Вязов <vjazov@yamoney.ru>
2016-02-10 15:39:03 +0300

    Pod version and localization updated

--- Example/ios/Example2/BaseUI/Classes/YMALocalization.h
+++ Example/ios/Example2/BaseUI/Classes/YMALocalization.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-#define YMALocalizedString(key, comment) \
+#define NSLocalizedString(key, comment) \
         [YMALocalization stringByKey:(key)]
 
 #define YMALocalizedImage(key, comment) \
